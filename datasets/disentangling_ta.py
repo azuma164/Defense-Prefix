e8046239607c3a19404ed51d757887957c8d73ed
derjustus <72736286+MassEast@users.noreply.github.com>
2025-09-11 11:31:53 +0200

    fix Materzynska dataset

--- datasets/disentangling_ta.py
+++ datasets/disentangling_ta.py
@@ -9,16 +9,18 @@ class DisentanglingDataset(Dataset):
     def __init__(self, root, transform=None):
         root_dir = os.path.join(root, 'typographic_attack_dataset')
         annotations_file = os.path.join(root_dir, 'annotations.json')
-        classes_file =  os.path.join(root_dir, 'classes.json')
         img_dir = os.path.join(root_dir, 'images')
 
         self.img_dir = img_dir 
         self.transform = transform 
         with open(annotations_file) as f:
             self.annotations = json.load(f)
-        with open(classes_file) as f:
-            self.classes_dict = json.load(f)
-        self.classes = list(self.classes_dict.keys())
+
+        unique_classes = set()
+        for img_data in self.annotations.values():
+            unique_classes.add(img_data["true object"])
+        self.classes = list(unique_classes)
+
         self.templates = ["a photo of {}."]
         self.img_files = []
         for key in self.annotations:
@@ -35,6 +37,6 @@ class DisentanglingDataset(Dataset):
             image = self.transform(image)
             
         true_label = self.annotations[img_file]['true object']
-        self.annotations[img_file]['typographic attack label']
+        # self.annotations[img_file]['typographic attack label']
 
         return (image, image, self.classes.index(true_label))
